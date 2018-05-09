a8ec59772c0a1a96229aa104c8905f0a86af7c24
LACY HOGAN <lacy.hogan@gmail.com>
2018-05-08 20:03:45 -0700

    Pushing lab 17

--- src/model/account.js
+++ src/model/account.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import crypto from 'crypto';
 import jsonWebToken from 'jsonwebtoken';
+import HttpError from 'http-errors';
 
 const HASH_ROUNDS = 8;
 const TOKEN_SEED_LENGTH = 128;
@@ -11,16 +12,20 @@ const TOKEN_SEED_LENGTH = 128;
 const accountSchema = mongoose.Schema({
   passwordHash: {
     type: String,
-    require: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
   },
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   tokenSeed: {
     type: String,
-    require: true,
+    required: true,
     unique: true,
   },
   createdOn: {
@@ -28,6 +33,16 @@ const accountSchema = mongoose.Schema({
     default: () => new Date(),
   },
 });
+
+function pVerifyPassword(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then((result) => {
+      if (!result) {
+        throw new HttpError(400, 'AUTH - incorrect data');
+      }
+      return this; 
+    });
+}
 
 function pCreateToken() {
   this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
@@ -41,6 +56,7 @@ function pCreateToken() {
 }
 
 accountSchema.methods.pCreateToken = pCreateToken;
+accountSchema.methods.pVerifyPassword = pVerifyPassword;
 
 const Account = mongoose.model('account', accountSchema);
 
--- src/route/auth-router.js
+++ src/route/auth-router.js
@@ -5,6 +5,7 @@ import { json } from 'body-parser';
 import HttpErrors from 'http-errors';
 import Account from '../model/account';
 import logger from '../lib/logger';
+import basicAuthMiddleware from '../lib/basic-auth-middleware';
 
 const authRouter = new Router();
 const jsonParser = json();
@@ -22,6 +23,19 @@ authRouter.post('/signup', jsonParser, (request, response, next) => {
     })
     .then((token) => {
       logger.log(logger.INFO, 'AUTH - returning a 200 code and a token');
+      return response.json({ token });
+    })
+    .catch(next);
+});
+
+authRouter.get('/login', basicAuthMiddleware, (request, response, next) => {
+  if (!request.account) {
+    return next(new HttpErrors(400, 'AUTH - invalid request'));
+  }
+
+  return request.account.pCreateToken()
+    .then((token) => {
+      logger.log(logger.INFO, 'LOGIN - responding with a 200 status and a Token');
       return response.json({ token });
     })
     .catch(next);
